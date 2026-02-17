959a24826e9acd69b1c6007f9d2eb2e0b0f389c3
talhaahmed4399-boop <talhaahmed4399@gmail.com>
2026-02-17 15:40:11 -0800

    Create tournement.tsx

---
+++ PKL Esports hub/src/pages/tournement.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tournaments: React.FC = () => (
+  <div>
+    <h1 className="text-3xl font-heading-font text-yellow-500 mb-4">Tournaments</h1>
+    <p>Manage tournament registrations here.</p>
+  </div>
+);
+
+export default Tournaments;
