dbda2187be48dd5dae2243cef31a38e99599f170
talhaahmed4399-boop <talhaahmed4399@gmail.com>
2026-02-17 15:39:03 -0800

    Create teams.tsx

---
+++ PKL Esports hub/src/pages/teams.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Teams: React.FC = () => (
+  <div>
+    <h1 className="text-3xl font-heading-font text-yellow-500 mb-4">Teams</h1>
+    <p>Here you can view all your teams.</p>
+  </div>
+);
+
+export default Teams;
