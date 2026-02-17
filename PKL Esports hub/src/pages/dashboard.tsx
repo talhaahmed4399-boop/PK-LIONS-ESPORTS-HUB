ba81e109b733b561b9651a4d17ef211082c77423
talhaahmed4399-boop <talhaahmed4399@gmail.com>
2026-02-17 15:38:19 -0800

    Create dashboard.tsx

---
+++ PKL Esports hub/src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => (
+  <div>
+    <h1 className="text-3xl font-heading-font text-yellow-500 mb-4">Dashboard</h1>
+    <p>Welcome to PKL Esports Hub!</p>
+  </div>
+);
+
+export default Dashboard;
